05a0a763114350509f32561b3d4e68332d5f4311
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-01-25 10:23:06 +0100

    Add webpack configuration and header component

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: config => {
+    config.resolve.fallback = {
+      aws4: false, // suppress mongo aws4 warning that fires each time
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ next/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='flex flex-row w-full h-20 p-4 items-center justify-start shadow-lg'>
+      <h1>QA Toolkit</h1>
+    </div>
+  );
+};
+
+export default Header;
